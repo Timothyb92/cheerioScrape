223447bafa7e42b9e70043da749e4346f38cac30
Timothy Bedsole <timbedsole@gmail.com>
2018-09-10 15:25:36 -0400

    Properly scraping indeed job search and setting desired values to an object

--- server.js
+++ server.js
@@ -21,10 +21,27 @@ app.engine(
 app.set('view engine', 'handlebars');
 mongoose.connect("mongodb://localhost/cheerioScraper", { useNewUrlParser: true });
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.render('index');
-})
+});
 
-app.listen(PORT, function() {
+app.get('/scrape', (req, res) => {
+    axios.get('https://www.indeed.com/jobs?q=full+stack+developer&l=Portland%2C+OR').then((response) => {
+        var $ = cheerio.load(response.data);
+        var results = {};
+        $('div.result').each((i, element) => {
+            var jobTitle = $(element).find('a').attr('title');
+            var link = 'https://indeed.com/' + $(element).find('a').attr('href');
+            var company = $(element).find("span.company").text().slice(9);
+
+            results.company = company;
+            results.link = link;
+            results.position = jobTitle;
+        });
+        console.log(results);
+    });
+});
+
+app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
 })
