8039edc1055bcbda6031f7f1d8472608cb6c557f
Timothy Bedsole <timbedsole@gmail.com>
2018-09-10 23:05:08 -0400

    Added a render function when there are no jobs yet scraped

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ $(() => {
         //Use a function to display each job posting
         renderJobs(results);
       } else {
-        console.log(`No results. Need to scrape!`);
+        renderNone();
         //Use a function to display divs when there are no job postings
       }
     });
@@ -45,6 +45,44 @@ $(() => {
       jobContainer.append(card);
     }
   };
+
+  function renderNone() {
+    var emptyContainer = $(
+      [
+        '<div class="alert alert-danger text-center">',
+        "<h4>No jobs have been scraped yet! Get scrapin' or check out your saved job listings.</h4>",
+        '</div>',
+        '<div class="card">',
+        '<div class="card-header text-center">',
+        '<h3>What Would You Like To Do?</h3>',
+        '</div>',
+        '<div class="card-body text-center">',
+        '<h4><a href="/scrape" class="startScrape emptyButton">Try Scraping New Articles</a></h4>',
+        '<h4><a href="/saved" class="goToSaved emptyButton">Go to Saved Articles</a></h4>',
+        '</div>',
+        '</div>'
+      ].join('')
+    );
+    jobContainer.append(emptyContainer);
+    // jobContainer.append(
+    //   '<div class="alert alert-danger text-center>',
+    //     "<h4>No jobs have been scraped yet! Get scrapin' or check out your saved job listings.",
+    //   '</div>',
+    //   '<div class="card">',
+    //     '<div class="card-header text-center">',
+    //       '<h3>What would you like to do?</h3>',
+    //     '</div>',
+    //     '<div class="card-body text-center">',
+    //       '<h4><a class="btn startScrape">Scrape For Some Jobs!</a></h4>',
+    //       '<h4><a class="btn goToSaved">Go To Saved Jobs!</a></h4>',
+    //     '</div>',
+    //   '</div>'
+    // );
+  };
+
+  $(document).on('click', '.saveJob', function() {
+    console.log($(this).data());
+  })
 
   renderPage();
 })
